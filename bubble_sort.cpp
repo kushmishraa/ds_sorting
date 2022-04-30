1cdb4f5b1f1e9b468c4b5d648959d2fe303cbf31
kush <mishrash64@gmail.com>
2022-04-30 20:23:55 +0530

    added bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+        int arr[10],n;
+
+        cout<<"number of elements =";
+        cin>>n;
+        for(int i=0 ; i<n ; i++){
+
+
+            cin>>arr[i];
+
+
+        }
+
+        for(int i=0 ; i<n-1 ; i++){
+
+                for(int j=0 ; j<n-i-1 ; j++){
+
+
+                        if(arr[j]>arr[j+1]){
+
+                                int temp= arr[j+1];
+                                arr[j+1]=arr[j];
+                                arr[j]=temp;
+
+
+                        }
+
+                }
+
+        }
+        for(int i=0 ; i<n ; i++){
+
+                cout<<arr[i];
+
+        }
+
+}
