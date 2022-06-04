6c06387ab578c4e06fe3032a625639ed3233a6d5
kush <mishrash64@gmail.com>
2022-06-04 17:36:43 +0530

    re arranged quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,90 +2,86 @@
 
 using namespace std;
 
-void swap(int arr[] , int i , int j){
+void swap(int arr[] , int i , int j ){
 
-    int temp = arr[i];
-    
-    arr[i]=arr[j];
-    arr[j]=temp;
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=arr[i];
 
 
 }
 
-int partitioin(int arr[] , int l , int r){
-
+int partition(int arr[] ,int l , int r){
 
         int pivot = arr[r];
-
         int i=l-1;
 
-        for(int j=l ;j<r; j++){
+        for(int j=l ; j<r ; j++){
 
-                if(arr[j] < pivot){
-
+                if(arr[j]<pivot){
+                        
                         i++;
                         swap(arr,i,j);
-
                 }
-
         }
 
         swap(arr,i+1,r);
 
         return i+1;
 
+
 }
 
-void quicksort(int arr[] , int l , int r){
+void quicksort(int arr[] ,int l , int r){
+
+                if(l<r){
+
+                        int pi = partition(arr,l,r);
+
+                        quicksort(arr,l,pi-1);
+                        quicksort(arr,pi+1,r);
 
 
-        if(l<r){
-
-                int pi = partitioin(arr,l,r);
-
-                quicksort(arr,l,pi-1);
-                quicksort(arr,pi+1,r);
 
 
-        }
+                }
 
 
 }
 
 int main(){
 
-    int arr[10],l,r,n;
+        int l,r,arr[10],n;
 
-    cout<<"elements : ";
-    cin>>n;
-    l=0;
-    r=n-1;
+        cout<<"enter number of elements in the array :";
+        cin>>n;
+        l=0;
+        r=n-1;
 
-    for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<n ; i++){
 
-        cin>>arr[i];
-    }
+                cin>>arr[i];
+        }
 
-    cout<<"\n===before sorting======\n";
+        cout<<"\n=====befor swapping=====\n";
 
-    for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<n ; i++){
 
-        cout<<arr[i];
-   }
-   
-    cout<<"\n===before sorting======\n";
+                cout<<arr[i];
+        }
 
+        cout<<"\n======before swapping====\n";
 
-    quicksort(arr,l,r);
+        quicksort(arr,l,r);
 
+        cout<<"\n========after swapping =======\n";
 
-    cout<<"\n===after sorting======\n";
+        for(int i=0 ; i<n ; i++){
 
-    for(int i=0 ; i<n ; i++){
+                cout<<arr[i];
+        }
 
-        cout<<arr[i];
-    }
+        cout<<"\n======after swapping======\n";
 
-    cout<<"\n===after sorting======\n";
 
 }
